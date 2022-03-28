3a2fd98e0fdb56af7bddbf588970c11a8db4909d
Ramzi Dakhmouche <57569833+Ramzisofo@users.noreply.github.com>
2022-03-28 22:00:46 +0200

    Update train_agent.py

--- src/train_agent.py
+++ src/train_agent.py
@@ -6,21 +6,19 @@ Script for training and saving the agent.
 def train(model, env, config):
     pass
 
-# Discuss buffer update
-def train_loop( environment, 
+# TODO: Discuss buffer update
+def interaction_loop( environment, 
                       agent,
                       rng,
                       num_episodes=None,
                       num_steps=None,
-                      logger_time_delta=1.,
-                      label='training_loop',
-                      log_loss=False,
                       buffer_capacity=50, 
                       number_updates=5, 
-                      batch_size=10
+                      batch_size=10, 
+                      nb_updated_transitions=2
                       ):
   """Perform the interaction loop.
-
+  
   Run the environment loop for `num_episodes` episodes. Each episode is itself
   a loop which interacts first with the environment to get an observation and
   then give that observation to the agent in order to retrieve an action. Upon
@@ -39,6 +37,7 @@ def train_loop( environment,
     buffer_capacity: minimum number of samples before updating the model 
     number_updates: number of updates from the same buffer
     batch_size: size of the training batch
+    nb_updated_transitions: (after the buffer is filled completely) number of updated transitions to make before updating the model
   """
   # logger = loggers.TerminalLogger(label=label, time_delta=logger_time_delta)
   iterator = range(num_episodes) if num_episodes else itertools.count()
@@ -62,6 +61,9 @@ def train_loop( environment,
     # Reset environment
     timestep = environment.reset()
 
+    # number of updated transitions 
+    nb_up_transitions = 0 
+
     # Run an episode.
     while not timestep.last():
       
@@ -70,15 +72,17 @@ def train_loop( environment,
 
       # Generate an action from the agent's policy and step the environment.
       mu, sigma = agent.apply_policy(learner_state.params.policy, obs) 
-      stand_gaussian = jax.random.normal(rng, (agent.action_dim,))
+      stand_gaussian = jax.random.normal(rng, (agent.action_dim,)) 
       action = (mu + sigma @ stand_gaussian)
       timestep = environment.step(action) 
 
       # store transition 
-      agent.buffer.store(obs,  action, timestep.reward, timestep.observation)
+      agent.buffer.store(obs,  action, timestep.reward, timestep.observation, False)
+      nb_up_transitions += 1
 
-      if agent.buffer.count >= buffer_capacity:
+      if agent.buffer.__len__() >= buffer_capacity and nb_up_transitions >= nb_updated_transitions:
 
+          nb_up_transitions = 0
           for nb_updates in range(number_updates): 
 
               transitions = agent.buffer.sample(batch_size)
@@ -87,11 +91,11 @@ def train_loop( environment,
               all_logs.append(logs)
 
       # Book-keeping.
-      episode_steps += 1
-      num_total_steps += 1
+      # episode_steps += 1
+      # num_total_steps += 1
       episode_return += timestep.reward
 
     # store returns
     all_returns.append(episode_return)
 
-  return all_returns, all_logs 
+  return all_returns, all_logs, num_total_steps
